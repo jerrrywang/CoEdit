52131a4647e536520e7f52039f57828a873c0979
Jerry <wangjerr@usc.edu>
2018-07-16 10:43:28 -0700

    Install draft-js

---
+++ src/components/textEditor/TextEditor.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Editor, EditorState} from 'draft-js';
+
+export default class TextEditor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            editorState: EditorState.createEmpty()
+        };
+    }
+    onChange = (editorState) => this.setState({editorState});
+
+    render() {
+        return (
+            <Editor editorState={this.state.editorState} onChange={() => this.onChange()} />
+        );
+    }
+}
