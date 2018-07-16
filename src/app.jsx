ecb1f843b4c867f6b547971fbf1a8d94a786d51f
Jerry <wangjerr@usc.edu>
2018-07-16 11:22:21 -0700

    Add text editor styles

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import TextEditor from './components/textEditor/TextEditor';
 
 export default class App extends React.Component {
   render() {
-    return (<div>
-      <h2>Welcome to React!</h2>
-    </div>);
+    const styles = {
+      main: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+    };
+    return (
+        <div style={styles.main}>
+          <h2>Text editor!</h2>
+          <TextEditor />
+        </div>
+    );
   }
 }
--- src/components/textEditor/TextEditor.jsx
+++ src/components/textEditor/TextEditor.jsx
@@ -1,19 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Editor, EditorState} from 'draft-js';
+import {Editor, EditorState, RichUtils} from 'draft-js';
 
 export default class TextEditor extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            editorState: EditorState.createEmpty()
-        };
+        this.state = {editorState: EditorState.createEmpty()};
+        this.onChange = (editorState) => this.setState({editorState});
     }
-    onChange = (editorState) => this.setState({editorState});
+
+    _onBoldClick(e) {
+        e.preventDefault()
+        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+    }
 
     render() {
+        const styles = {
+            content: {
+                width: 480,
+                margin: 'auto',
+            },
+            editor: {
+                border: '1px solid black',
+                height: 200
+            },
+        };
         return (
-            <Editor editorState={this.state.editorState} onChange={() => this.onChange()} />
-        );
+            <div style = {styles.content}>
+                <button onMouseDown={(e) => this._onBoldClick(e)}>BOLD</button>
+                <div style = {styles.editor}>
+                    <Editor
+                        editorState={this.state.editorState}
+                        onChange={this.onChange}
+                    />
+                </div>
+        </div>);
     }
 }
