90499ec24ff435473c90bfe5536ff386ae9903c5
benklenk <benklenk@outlook.com>
2018-07-16 11:13:10 -0700

    add socket.io & express dependencies

--- src/components/textEditor/TextEditor.jsx
+++ src/components/textEditor/TextEditor.jsx
@@ -3,17 +3,18 @@ import ReactDOM from 'react-dom';
 import {Editor, EditorState} from 'draft-js';
 
 export default class TextEditor extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            editorState: EditorState.createEmpty()
-        };
-    }
-    onChange = (editorState) => this.setState({editorState});
+  constructor(props) {
+    super(props);
+    this.state = {
+      editorState: EditorState.createEmpty()
+    };
+  }
 
-    render() {
-        return (
-            <Editor editorState={this.state.editorState} onChange={() => this.onChange()} />
-        );
-    }
+  onChange(editorState) {
+    this.setState({ editorState});
+  }
+
+  render() {
+    return (<Editor editorState={this.state.editorState} onChange={(editorState) => this.onChange(editorState)}/>);
+  }
 }
